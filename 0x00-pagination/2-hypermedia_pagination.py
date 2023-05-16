ff7e1aab6bdaf24d6bd9f1767cbdf54912941c6d
Ken-Obieze <kenneth.obieze@outlook.com>
2023-05-16 02:09:03 +0100

    task2n3

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""Module to Implement a get_hyper method."""
+import csv
+import math
+from typing import List, Dict, Union
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return the corresponding page of the dataset for the given pagination parameters.
+        """
+        assert isinstance(page, int) and page > 0, "page must be an integer greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, "page_size must be an integer greater than 0"
+
+        start, end = index_range(page, page_size)
+        dataset = self.dataset()
+        if start >= len(dataset):
+            return []
+
+        return dataset[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Return a dictionary containing the pagination information for the given parameters.
+        """
+        assert isinstance(page, int) and page > 0, "page must be an integer greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, "page_size must be an integer greater than 0"
+
+        dataset = self.dataset()
+        total_pages = math.ceil(len(dataset) / page_size)
+        start, end = index_range(page, page_size)
+
+        if start >= len(dataset):
+            return {
+                "page_size": 0,
+                "page": page,
+                "data": [],
+                "next_page": None,
+                "prev_page": None,
+                "total_pages": total_pages
+            }
+
+        data = dataset[start:end]
+        next_page = page + 1 if end < len(dataset) else None
+        prev_page = page - 1 if start > 0 else None
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+        }
---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import Dict, List
+
+
+class Server:
+    """
+    Server class to paginate a database of popular baby names.
+    """
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0"""
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        Return a dictionary with the following key-value pairs:
+        index: the current start index of the return page.
+        next_index: the next index to query with.
+        page_size: the current page size.
+        data: the actual page of the dataset.
+        """
+        dataset_length = len(self.dataset())
+
+        if index is None:
+            index = 0
+
+        assert index >= 0 and index < dataset_length, "Index out of range"
+
+        start = index
+        end = start + page_size
+        dataset_page = self.dataset()[start:end]
+
+        # Check if any rows have been deleted from the dataset
+        deleted_rows = []
+        for i, row in enumerate(dataset_page):
+            row_index = start + i
+            if row_index not in self.indexed_dataset():
+                deleted_rows.append(i)
+
+        # If any rows have been deleted, recalculate the page
+        if deleted_rows:
+            new_start = start + len(deleted_rows)
+            new_end = new_start + page_size
+            dataset_page = self.dataset()[new_start:new_end]
+
+        next_index = end if end < dataset_length else None
+
+        return {
+            "index": start,
+            "next_index": next_index,
+            "page_size": page_size,
+            "data": dataset_page,
+        }
