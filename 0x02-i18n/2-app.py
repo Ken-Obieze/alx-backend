2cf9cdb2f64a1229372bd569cb28ad0e675a4503
ken-obieze <kenneth.obieze@outlook.com>
2023-10-31 21:51:05 +0000

    task 2 fix

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -28,9 +28,7 @@ def get_locale():
 @app.route('/')
 def index():
     """Renders the index.html template."""
-    return render_template('2-index.html', \
-        title=gettext('Welcome to Holberton'), \
-        header=gettext('Hello world'))
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
